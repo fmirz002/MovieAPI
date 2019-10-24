0736f053bbc8ac2d244f7513f48d7f6174865bf0
Faiz Mirza <faiz.mirza@jpl.nasa.gov>
2019-10-24 00:57:39 -0700

    Cleanup debug statements, add similar resource for shows

--- app.py
+++ app.py
@@ -14,17 +14,32 @@ class Movie(Resource):
             return {message: "Incorrectly formatted request"}, 400
         if id[0] != '$' or id[1] != '{' or id[-1] != '}':
             return {message: "Incorrectly formatted request"}, 400
-        print(id)
         id = id.split('{')[1].strip('}')
-        print(id)
+        if id.isdigit() == False:
+            return {message: "Incorrectly formatted request"}, 400
         URL = 'https://api.themoviedb.org/3/movie/' + id
         PARAMS = {'api_key': os.environ['MOVIE_API_KEY'], 'language':'en-US'}
-        print(PARAMS)
+        r = requests.get(url = URL, params = PARAMS)
+        data = r.json()  
+        return data, 200
+
+class Show(Resource):
+    def get(self, id): 
+        if len(id) < 3:
+            return {message: "Incorrectly formatted request"}, 400
+        if id[0] != '$' or id[1] != '{' or id[-1] != '}':
+            return {message: "Incorrectly formatted request"}, 400
+        id = id.split('{')[1].strip('}')
+        if id.isdigit() == False:
+            return {message: "Incorrectly formatted request"}, 400
+        URL = 'https://api.themoviedb.org/3/tv/' + id
+        PARAMS = {'api_key': os.environ['MOVIE_API_KEY'], 'language':'en-US'}
         r = requests.get(url = URL, params = PARAMS)
         data = r.json()  
         return data, 200
 
 api.add_resource(Movie, "/movie/<string:id>")
+api.add_resource(Show, "/movie/<string:id>")
 
 if __name__ == "__main__":
   app.run()
