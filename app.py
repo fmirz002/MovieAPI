6c01eea67d24d6033b7531364b89bbee7ddd9d27
Faiz Mirza <faiz.mirza@jpl.nasa.gov>
2019-10-24 00:49:26 -0700

    Add some debugging statements

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ api = Api(app)
 class Movie(Resource):
 
     def get(self, id): 
+        if len(id) < 3:
+            return {message: "Incorrectly formatted request"}, 400
+        if id[0] != '$' or id[1] != '{' or id[-1] != '}':
+            return {message: "Incorrectly formatted request"}, 400
+        print(id)
         id = id.split('{')[1].strip('}')
+        print(id)
         URL = 'https://api.themoviedb.org/3/movie/' + id
         PARAMS = {'api_key': os.environ['MOVIE_API_KEY'], 'language':'en-US'}
+        print(params)
         r = requests.get(url = URL, params = PARAMS)
         data = r.json()  
         return data, 200
