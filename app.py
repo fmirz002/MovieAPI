7a5c2a9af644ce4d1383734d1e33180e3cc00720
Faiz Mirza <faiz.mirza@jpl.nasa.gov>
2019-10-24 00:44:56 -0700

    Add first pass at /movie/

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_restful import Api
+import os
 
 app = Flask(__name__)
 api = Api(app)
+
+class Movie(Resource):
+
+    def get(self, id): 
+        id = id.split('{')[1].strip('}')
+        URL = 'https://api.themoviedb.org/3/movie/' + id
+        PARAMS = {'api_key': os.environ['MOVIE_API_KEY'], 'language':'en-US'}
+        r = requests.get(url = URL, params = PARAMS)
+        data = r.json()  
+        return data, 200
 
 if __name__ == "__main__":
   app.run()
