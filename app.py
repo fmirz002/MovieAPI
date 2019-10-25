c2fd6c322bbccfceff1813d20e79a3318824716e
Faiz Mirza <faiz.mirza@jpl.nasa.gov>
2019-10-25 02:40:06 -0700

    Add a first pass at the search, currenly only does movies

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
-class Movie(Resource):
+class Search(Resource):
+    def get(self): 
+        if 'query' not in request.args:
+            return {message: "Incorrectly formatted request"}, 400
+        query = request.args['query']
+        PARAMS = {'api_key':os.environ['MOVIE_API_KEY'], 'language':'en-US', 'page':'1', 'query':query}
+        r = requests.get(url = 'https://api.themoviedb.org/3/search/movie', params = PARAMS)
+        data = r.json()
+        return data, 200
 
+class Movie(Resource):
     def get(self, id): 
         if len(id) < 3:
             return {message: "Incorrectly formatted request"}, 400
@@ -40,6 +49,7 @@ class Show(Resource):
 
 api.add_resource(Movie, "/movie/<string:id>")
 api.add_resource(Show, "/movie/<string:id>")
+api.add_resource(Search, "/search")
 
 if __name__ == "__main__":
   app.run()
